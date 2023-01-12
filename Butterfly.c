1b396000923922cd83f2ab6439d49d5aaa979f33
Sourabh <developrr.sourabh@gmail.com>
2023-01-12 09:26:31 +0530

    add new stylew

--- Butterfly.c
+++ Butterfly.c
@@ -1,45 +1,61 @@
-#include<stdio.h>
+// #include<stdio.h>
 
-void main(){
-    // FIRST---PART
-    for (int i = 1; i <= 5; i++)
+// void main(){
+//     // FIRST---PART
+//     for (int i = 1; i <= 5; i++)
+//     {
+//   for (int j = 1; j <= i; j++)
+//   {
+//   printf("*");
+//   }
+
+//   for (int j = 1; j <= 2 * (5-i); j++)
+//   {
+//     printf(" ");
+//   }
+
+//   for (int j = 1; j <= i; j++)
+//   {
+//    printf("*");
+//   }
+//   printf("\n");
+
+//     }
+
+//      // SECOND---PART
+//   for (int i = 5; i >=1; i--)
+//   {
+
+//   for (int j = 1; j <= i; j++)
+//   {
+//     printf("*");
+//   }
+//   for (int j = 0; j < 2 * (5-i); j++)
+//   {
+//   printf(" ");
+//   }
+//   for (int j = 1; j <= i ; j++)
+//   {
+//     printf("*");
+//   }
+//   printf("\n");
+//   }
+
+// }
+
+// MY_CODE---START
+
+#include <stdio.h>
+
+void main()
+{
+  // FIRST_LOOP---START
+  for (int i = 0; i < 5; i++)
+  {
+    for (int j = 0; j <= i; j++)
     {
-  for (int j = 1; j <= i; j++)
-  {
-  printf("*");
-  }
-
-  for (int j = 1; j <= 2 * (5-i); j++)
-  {
-    printf(" ");
-  }
-
-  for (int j = 1; j <= i; j++)
-  {
-   printf("*");
-  }
-  printf("\n");
-    
+      printf("*");
     }
-
-
-     // SECOND---PART
-  for (int i = 5; i >=1; i--)
-  {
-  
-  for (int j = 1; j <= i; j++)
-  {
-    printf("*");
+    printf("\n");
   }
-  for (int j = 0; j < 2 * (5-i); j++)
-  {
-  printf(" ");
-  }
-  for (int j = 1; j <= i ; j++)
-  {
-    printf("*");
-  }
-  printf("\n");
-  }
-  
 }
